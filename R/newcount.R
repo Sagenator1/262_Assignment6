489d2ad5d7ca6bb9ae5571cc09eaf8c933379083
Sage <sage@ucsb.edu>
2023-03-15 10:41:51 -0700

    Initial Committ

---
+++ R/newcount.R
@@ -0,0 +1,9 @@
+#' Sun data for plant growth
+#'
+#' Data for plant_growth_function for random number of weekly hours each week for a year
+#'
+#' @format A vector of sun data
+#' \itemize{
+#'  \item data for number}
+#'
+"newcount"
